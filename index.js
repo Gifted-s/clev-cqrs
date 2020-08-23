f3e4cec993e68fbca56074db625e7aae99e5567a
sunkanmiadewumi@gmail.com <sunkanmiadewumi1@gmail.com>
2020-08-23 02:08:33 +0100

    edited index.js

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ let makeCommand = (function () {
   _this.schema = {}
   _this.setSchema = function (Schema) {
     _this.schema = Schema
+    _this.schemaName = Schema.schemaName || 'unknown'
   }
   _this.Schema = Schema
   _this.View = View
   const commandHandlers = require('./src/commandHandlers')
   _this.eventStore = require('./src/eventStore') || []
-  _this.schemaName = _this.schema.schemaName || 'unknown'
   _this.handler = commandHandlers.handlers
   return _this
 })()
